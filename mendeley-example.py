644463b63c76fa2b1303b8ad163ca09d15bc0464
Davo00 <davit.melkonyan@sap.com>
2024-06-10 13:43:58 +0200

    Mendeley Downloader for a user

--- mendeley-example.py
+++ mendeley-example.py
@@ -1,12 +1,17 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, send_file
 import yaml
+
+import zipfile
+import os
+from io import BytesIO
 
 from mendeley import Mendeley
 from mendeley.session import MendeleySession
+from datetime import datetime
 
 
 with open('config.yml') as f:
-    config = yaml.load(f)
+    config = yaml.safe_load(f)
 
 REDIRECT_URI = 'http://localhost:5000/oauth'
 
@@ -23,7 +28,9 @@ def home():
         return redirect('/listDocuments')
 
     auth = mendeley.start_authorization_code_flow()
+    print(session)
     session['state'] = auth.state
+    
 
     return render_template('home.html', login_url=(auth.get_login_url()))
 
@@ -86,10 +93,34 @@ def download():
     mendeley_session = get_session_from_cookies()
 
     document_id = request.args.get('document_id')
+    print(document_id)
     doc = mendeley_session.documents.get(document_id)
     doc_file = doc.files.list().items[0]
 
     return redirect(doc_file.download_url)
+
+
+@app.route('/downloadAll')
+def download_all():
+    if 'token' not in session:
+        return redirect('/')
+
+    mendeley_session = get_session_from_cookies()
+    docs = mendeley_session.documents.list(view='client').items
+
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for doc in docs:
+            if not doc.files.list().items:
+                continue
+            print(doc.files.list().items)
+            doc_file = doc.files.list().items[0]
+            file_content = doc_file.download_url
+            zip_file.writestr(f"{doc.title}.pdf", file_content)
+
+    zip_buffer.seek(0)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    return send_file(zip_buffer, as_attachment=True, download_name=f'all_documents_{timestamp}.zip')
 
 
 @app.route('/logout')
